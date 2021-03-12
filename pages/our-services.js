ab0ce479d3905581c3b3df5d12292405668a9541
carlosworld <carlosworld93@gmail.com>
2021-03-12 11:04:59 -0600

    actualizacion

--- pages/our-services.js
+++ pages/our-services.js
@@ -1,5 +1,130 @@
 import React from "react";
 
 export default function OurServices() {
-  return <div>Our Services</div>;
+  return (
+    <>
+  {/* <PageTitleBox /> */}
+  <div className="about-box-main">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-6">
+              <h2 className="noo-sh-title">
+                Limpieza industrial,<span>comercial y recidencial</span>
+              </h2>
+              <p>
+                PARA OBRAS DE CONSTRUCCIÓN.
+              </p>
+              <p>
+                OFICINAS, BODEGAS, CLINICAS Y HOSPITALES.
+              </p>
+              <p>
+                EMPRESAS, NEGOCIOS, BANCOS.
+              </p>
+              <p>
+                LUGARES PÚBLICOS, EVENTOS, NAVES INDUSTRIALES Y OTROS.
+              </p>
+            </div>
+            <div className="col-lg-6">
+              <div className="banner-frame">
+                <img
+                  className="img-thumbnail img-fluid"
+                  src="images/07.jpg"
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+          <div className="row">
+          <div className="col-lg-6">
+              <div className="banner-frame">
+                <img
+                  className="img-thumbnail img-fluid"
+                  src="images/08.jpg"
+                  alt=""
+                />
+              </div>
+            </div>
+            <div className="col-lg-6">
+              <h2 className="noo-sh-title">
+                Fumigacion de espacios<span></span>
+              </h2>
+              <p>
+                FUMIGACIÓN DE CUCARACHAS
+              </p>
+              <p>
+                FUMIGACIÓN DE CHINCHES
+              </p>
+              <p>
+                FUMIGACIÓN DE TERMITA
+              </p>
+              <p>
+                FUMIGACIÓN DE ARAÑA
+              </p>
+              <p>
+                FUMIGACIÓN PARA EVITAR ROEDORES
+              </p>
+            </div>
+          </div>
+          <div className="row">
+
+            <div className="col-lg-6">
+              <h2 className="noo-sh-title">
+              LIMPIEZA DE VIDRIOS EN ALTURA<span></span>
+              </h2>
+              <p>
+              LAVADO Y DESMANCHADO DE VIDRIOS.
+              </p>
+              <p>
+              LIMPIEZA DE LOUVERS Y PARASOLES DE ALUMINIO.
+              </p>
+              <p>
+              LIMPIEZA DE PANELES DE ALUMINIO (ALUCOBOND).
+              </p>
+              <p>
+              LIMPIEZA DE ESTRUCTURAS Y DOMOS.
+              </p>
+
+            </div>
+            <div className="col-lg-6">
+              <div className="banner-frame">
+                <img
+                  className="img-thumbnail img-fluid"
+                  src="images/09.jpg"
+                  alt=""
+                />
+              </div>
+            </div>
+
+          </div>
+          <div className="row my-5">
+            {/* <div className="col-sm-6 col-lg-4">
+              <div className="service-block-inner">
+                <h3>Calidad en el servicio</h3>
+                <p>
+                </p>
+              </div>
+            </div> */}
+            {/* <div className="col-sm-6 col-lg-4">
+              <div className="service-block-inner">
+                <h3>Mas de 30 años de experiencia</h3>
+                <p>
+
+                </p>
+              </div>
+            </div> */}
+            {/* <div className="col-sm-6 col-lg-4">
+              <div className="service-block-inner">
+                <h3>Personal capasitado y certificado</h3>
+                <p>
+
+                </p>
+              </div>
+            </div> */}
+          </div>
+          {/* <OurTeam /> */}
+        </div>
+      </div>
+
+  </>
+  );
 }
